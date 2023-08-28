c1987d679604c8081d830ebff1194973232c9d3b
Isabella Correa <correa.isasouza@gmail.com>
2023-08-28 17:22:41 -0300

    Prática: Eclipse, Java, Git e GitHub

---
+++ teste/src/teste/Teste1.java
@@ -0,0 +1,9 @@
+package teste;
+public class Teste1 {
+	public static void main(String[] args) {
+		System.out.print("Usando ");
+		System.out.println("print e println");
+		System.out.println("Fim do programa");
+		}}
+	
+	
