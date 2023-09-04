534c9a1579e7ee6d8a2ba933181dadd0cfd95e27
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 15:29:12 -0300

    Operadores binários - básico

---
+++ OperadoresBinarios.java
@@ -0,0 +1,15 @@
+package operadoresbi1;
+
+public class OperadoresBinarios {
+	public static void main(String args[]){
+	int num1 = 3, num2 = 5;
+
+	System.out.println("num1 = " + num1);
+	System.out.println("num2 = " + num2);
+	System.out.println("num1 + num2 = " + (num1 + num2));
+	System.out.println("num1 - num2 = " + (num1 - num2));
+	System.out.println("num1 * num2 = " + (num1 * num2));
+	System.out.println("num1 / num2 = " + (num1 / num2));
+	System.out.println("num1 % num2 = " + (num1 % num2));
+}
+}
