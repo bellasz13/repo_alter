2d2ccbd9a2a00461cb11282919374ecdd408a1ce
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 15:33:09 -0300

    Operadores Binários - Double

---
+++ OperadoresBinariosDouble.java
@@ -0,0 +1,15 @@
+package operadoresbidouble;
+
+public class OperadoresBinariosDouble {
+
+	public static void main(String args[]){
+		int num1 = 3, num2 = 5;
+
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+		System.out.println("(double) num1 / num2 = " + ((double) num1 / num2));
+		System.out.println("num1 / (double) num2 = " + (num1 / (double) num2));
+		System.out.println("(double) num1 / (double) num2 = " + ((double) num1 /
+		(double) num2));
+		}
+}
