fff008926a1f12e693dac8c8811ed5614f56bc3f
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 16:00:03 -0300

    Operadores Relacionais

---
+++ OperadorRelacional.java
@@ -0,0 +1,13 @@
+package operadorelac;
+
+public class OperadorRelacional {
+
+	public static void main(String args[]){
+		
+		System.out.println(5 > 2); //true
+		System.out.println(5 >= 2); //true
+		System.out.println(5 < 2); //false
+		System.out.println(5 <= 2); //false
+		System.out.println(5 != 2); //true
+		}
+}
