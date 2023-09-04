169a05693e6cb60545ecbb120be2ab15349f518a
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 14:30:18 -0300

    Saída - print e println

---
+++ Saida1.java
@@ -0,0 +1,10 @@
+package objdesaida;
+
+public class Saida1 {
+		public static void main(String args[]){
+		System.out.print("Usando ");
+		System.out.println("print e println");
+		System.out.println("Fim do programa");
+		}
+		}
+
