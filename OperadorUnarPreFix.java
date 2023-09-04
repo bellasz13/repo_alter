9d08139e2cf546f66234bc28d5e1953f024bdfee
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 15:53:33 -0300

    Operadores Unários Pré-fixados

---
+++ OperadorUnarPreFix.java
@@ -0,0 +1,13 @@
+package operadoresunarprefix;
+
+public class OperadorUnarPreFix {
+
+	public static void main(String[] args) {
+		int num = 5;
+		System.out.println(num); //5
+		System.out.println(++num); //6
+		System.out.println(num); //6
+		System.out.println(--num); //5
+		System.out.println(num); //5
+		}
+}
