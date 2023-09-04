15315a0fadda36b76f7b5d06397c812c05119bb2
Isabella Correa <correa.isasouza@gmail.com>
2023-09-04 10:17:42 -0300

    Prática: Eclipse, Java, Git e GitHub

--- teste/src/teste/Teste1.java
+++ teste/src/teste/Teste1.java
@@ -1,9 +1,13 @@
 package teste;
 public class Teste1 {
-	public static void main(String[] args) {
-		System.out.print("Usando ");
-		System.out.println("print e println");
-		System.out.println("Fim do programa");
-		}}
-	
-	
+public static void main(String args[]){
+	int num1 = 3, num2 = 5;
+
+	System.out.println("num1 = " + num1);
+	System.out.println("num2 = " + num2);
+	System.out.println("(double) num1 / num2 = " + ((double) num1 / num2));
+	System.out.println("num1 / (double) num2 = " + (num1 / (double) num2));
+	System.out.println("(double) num1 / (double) num2 = " + ((double) num1 /
+	(double) num2));
+}
+}
