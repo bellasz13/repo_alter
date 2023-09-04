de77ee059bb3d4345e033525e547963a12ae5b59
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 14:40:29 -0300

    Caixa de diálogo - Exemplo

---
+++ CaixaDeDialogo1.java
@@ -0,0 +1,12 @@
+package caixadialogoex;
+
+import javax.swing.JOptionPane;
+
+public class CaixaDeDialogo1 {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Exemplo de texto em uma janela!");
+		System.exit(0);
+		}
+		}
