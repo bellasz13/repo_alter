a5d9b1897b5d90acbe749c9975a5b526b161d0a4
bellasz13 <142807795+bellasz13@users.noreply.github.com>
2023-09-04 15:56:50 -0300

    Operadores Unários Pós-fixados

---
+++ OperadorUnarPosFix.java
@@ -0,0 +1,14 @@
+package operadorunarposfix;
+
+public class OperadorUnarPosFix {
+
+	public static void main(String[] args) {
+		int num = 5;
+		System.out.println(num); //5
+		System.out.println(num++); //5
+		System.out.println(num); //6
+		System.out.println(num--); //6
+		System.out.println(num); //5
+
+		}
+}
